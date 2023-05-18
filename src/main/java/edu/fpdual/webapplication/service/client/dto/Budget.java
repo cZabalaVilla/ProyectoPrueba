bb24a1c1fb963fe8b957502f54b97963fb6eb0cc
DIR\m.garzon.montesinos <mgarmon6@gmail.com>
2023-05-18 14:14:31 +0200

    Implementación Budget WebApp ver.2

--- src/main/java/edu/fpdual/webapplication/service/client/dto/Budget.java
+++ src/main/java/edu/fpdual/webapplication/service/client/dto/Budget.java
@@ -1,4 +1,27 @@
 package edu.fpdual.webapplication.service.client.dto;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@XmlRootElement
 public class Budget {
+    private int userID;
+    private int budgetID;
+    private String budgetName;
+    private String description;
+    private LocalDateTime creationDate;
+    //private Currency currency;
+    //private ArrayLis<Expense> expenses;
+
+    public Budget(String budgetName, String descripcion, LocalDateTime creationDate) {
+        this.budgetName = budgetName;
+        this.description = description;
+        this.creationDate = LocalDateTime.now();
+    }
+
 }
